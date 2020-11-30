cd2c55e64c85ecdae020345722d900c6524666be
ap-dev1 <64612312+ap-dev1@users.noreply.github.com>
2020-11-29 19:14:41 -0500

    test google analytics; delete stream once done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 // import logo from './logo.svg';
+import ReactGA from 'react-ga';
 import './App.css';
 import SubscribeMailchimp from './SubscribeMailchimp'
 
 function App() {
+  
+  // G-044X7R2Z81  or  UA-044X7R2Z81-G
+  ReactGA.initialize('UA-044X7R2Z81-G');
+  ReactGA.pageview('/homepage');
+
   return (
     <div className="App">
       <header className="App-header">
