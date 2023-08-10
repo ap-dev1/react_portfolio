8e29d675c8e3b5777907fe79cc565be7ef2ccbef
ap-dev1 <andrei.popa.dev@gmail.com>
2023-08-10 10:48:28 -0400

    ga4

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import "./App.sass";
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-MB2HPVP47E");
 
 ReactDOM.render(
   <React.StrictMode>
